75eb2275ba156a9ed6cdaf947f33b95d1e33577f
eric <48635802+ericziethen@users.noreply.github.com>
2020-11-29 15:16:27 +1100

    Fix linting issues

--- test_package/prog.py
+++ test_package/prog.py
@@ -1,10 +1,13 @@
+"""Test Module."""
 
 
 def sum_numbers(num1, num2):
+    """Sum the given numbers."""
     return num1 + num2
 
 
 def main():
+    """Run Main function."""
     num1 = 5
     num2 = 2
     print(F'Summing "{num1}" and "{num2}" equals "{sum_numbers(num1, num2)}"')
