76d79e3f140f1d2a9c30bf7cf023767021e0ac4c
eric <48635802+ericziethen@users.noreply.github.com>
2020-11-29 16:54:33 +1100

    misc10

--- tests/test_package/test_prog.py
+++ tests/test_package/test_prog.py
@@ -4,5 +4,13 @@ from test_package.prog import sum_numbers
 def test_sum_ok():
     assert sum_numbers(5, 6) == 11
 
+def test_sum_ok2():
+    assert sum_numbers(6, 6) == 12
+
+def test_sum_ok3():
+    assert sum_numbers(-6, 6) == 0
+
+'''
 def test_fails():
     assert sum_numbers(1, 3) == 42
+'''
